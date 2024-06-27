7a1ce3984dd5ca48d71f94a8b27722fcce8aa2d8
Ziek <zieksanchez3@gmail.com>
2024-06-27 10:01:38 -0700

    Update server code

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,8 @@ const app = express();
 app.use(
   cors({
     origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
